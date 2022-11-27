0d40171b59f789eafe8aa546606cebdbb825f78b
Emmy <32604310+emm218@users.noreply.github.com>
2022-11-27 15:56:16 -0500

    can open a window, so close to clearing the screen

---
+++ src/display.rs
@@ -0,0 +1,134 @@
+use std::error::Error;
+use std::num::NonZeroU32;
+
+use winit::{
+    window::Window,
+    platform::unix,
+    dpi::PhysicalSize,
+};
+use raw_window_handle::{
+    RawDisplayHandle,
+    HasRawDisplayHandle,
+    RawWindowHandle,
+    HasRawWindowHandle,
+};
+use glutin::{
+    config:: { Config, ConfigTemplateBuilder, GetGlConfig },
+    context::{ 
+        ContextApi,
+        ContextAttributesBuilder,
+        NotCurrentContext,
+        NotCurrentGlContextSurfaceAccessor,
+        PossiblyCurrentContext,
+        Version 
+    },
+    display::{ 
+        Display as GlutinDisplay, 
+        DisplayApiPreference, 
+        GlDisplay, 
+        GetGlDisplay 
+    },
+    error::Result as GlutinResult,
+    surface::{ Surface, SurfaceAttributesBuilder, WindowSurface },
+};
+
+mod renderer;
+
+use renderer::Renderer;
+
+pub struct Display {
+    pub window: Window,
+    context: PossiblyCurrentContext,
+    surface: Surface<WindowSurface>,
+    renderer: Renderer,
+}
+
+impl Display {
+    pub fn new(window: Window) -> Result<Self, Box<dyn Error>> {
+        let display = create_gl_display(window.raw_display_handle())?;
+        let config = pick_gl_config(&display, None)?;
+        let context = create_gl_context(&display, &config, None)?;
+        let surface = create_gl_surface(&context, window.inner_size(), window.raw_window_handle())?;
+        
+        let context = context.make_current(&surface)?;
+        
+        let mut renderer = Renderer::new(&context)?;
+        
+        renderer.resize(window.inner_size());
+
+        renderer.clear();
+
+        Ok(Self {
+            window,
+            context,
+            surface,
+            renderer
+        })
+    }
+}
+
+fn create_gl_display(
+    display_handle: RawDisplayHandle
+) -> GlutinResult<GlutinDisplay> {
+    
+    let preference = DisplayApiPreference::GlxThenEgl(
+        Box::new(unix::register_xlib_error_hook));
+    
+    unsafe { GlutinDisplay::new(display_handle, preference) }
+}
+
+fn pick_gl_config(
+    gl_display: &GlutinDisplay,
+    window_handle: Option<RawWindowHandle>,
+) -> Result<Config, String> {
+    
+    let mut default_config = ConfigTemplateBuilder::new();
+
+    if let Some(window_handle) = window_handle {
+        default_config = 
+            default_config.compatible_with_native_window(window_handle);
+    }
+    
+    let gl_config = unsafe {
+        gl_display.find_configs(default_config.build())
+            .ok()
+            .and_then(|mut configs| configs.next())
+    };
+
+    if let Some(gl_config) = gl_config {
+        return Ok(gl_config);
+    }
+
+    Err(String::from("failed to find a valid gl config"))
+}
+
+fn create_gl_context(
+    gl_display: &GlutinDisplay,
+    gl_config: &Config,
+    window_handle: Option<RawWindowHandle>
+) -> GlutinResult<NotCurrentContext> {
+    let context_attributes = ContextAttributesBuilder::new()
+        .with_context_api(ContextApi::OpenGl(Some(Version::new(3, 3))))
+        .build(window_handle);
+    
+    unsafe { gl_display.create_context(&gl_config, &context_attributes) }
+}
+
+fn create_gl_surface(
+    gl_context: &NotCurrentContext,
+    size: PhysicalSize<u32>,
+    window_handle: RawWindowHandle
+) -> GlutinResult<Surface<WindowSurface>> {
+    
+    let gl_display = gl_context.display();
+    let gl_config = gl_context.config();
+
+    let surface_attributes =
+        SurfaceAttributesBuilder::<WindowSurface>::new().build(
+            window_handle,
+            NonZeroU32::new(size.width).expect("width must be non-zero"),
+            NonZeroU32::new(size.height).expect("height must be non-zero")
+        );
+
+    unsafe { gl_display.create_window_surface(&gl_config, &surface_attributes) }
+} 
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,32 @@
-use glfw::{Action, Context, Key};
+use std::error::Error;
 
+use winit::{
+    event::{ Event, WindowEvent },
+    event_loop::EventLoop,
+    window::Window,
+};
 
+mod display;
 
-fn main() {
-    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS)
-        .expect("failed to initialize glfw");
+use display::Display;
 
-    let (mut window, events) = 
-        glfw.create_window(300, 300, "raytrace", glfw::WindowMode::Windowed)
-        .expect("failed to create glfw window");
-
-    window.set_key_polling(true);
-    window.make_current();
+fn main() -> Result<(),Box<dyn Error>> {
+    let event_loop = EventLoop::new();
+    let window = Window::new(&event_loop)?;
     
-    gl::load_with(|s| window.get_proc_address(s));
+    let display = Display::new(window)?;
     
-    unsafe {
-        gl::ClearColor(0.0, 0.0, 0.0, 1.0);
-    }
+    event_loop.run(move |event, _, control_flow| {
+        control_flow.set_wait();
 
-    while !window.should_close() {
-        unsafe {
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-        }
-        window.swap_buffers();
-        
-        glfw.poll_events();
-        for (_, event) in glfw::flush_messages(&events) {
-            handle_window_event(&mut window, event);
-        }
-    }
-}
-
-fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
-    match event {
-        glfw::WindowEvent::Key(Key::Q, _, Action::Press, _) => {
-            window.set_should_close(true)
-        }
-        glfw::WindowEvent::Size(width, height) => {
-            unsafe {
-                gl::Viewport(0, 0, width, height);
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                control_flow.set_exit();
             }
+            _ => ()
         }
-        _ => {} 
-    }
+    })
 }
